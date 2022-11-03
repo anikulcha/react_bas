7ca9a2118bac3b0412b332296c82723692b6173b
andrey <nikulcha1998@gmail.com>
2022-11-04 00:02:38 +0200

    messages block

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,27 @@ import style from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div className={style.content}>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={`${style.dialog} ${style.active}`}>Andrey</div>
+                <div className={style.dialog}>Tor</div>
+                <div className={style.dialog}>Tur</div>
+                <div className={style.dialog}>Zeus</div>
+                <div className={style.dialog}>Mars</div>
+                <div className={style.dialog}>MLRS Himars</div>
+                <div className={style.dialog}>Odin</div>
+                <div className={style.dialog}>Ra</div>
+                <div className={style.dialog}>Susanoo</div>
+                <div className={style.dialog}>Amaterasu</div>
+
+            </div>
+            <div className='messages'>
+                <div className="message">
+                    hi
+                </div>
+                <div className="message">my name is...</div>
+                <div className="message">Thunder rumbled out of a clear sky. and Mjolnir, Thor's hammer, slammed to the ground.</div>
+            </div>
         </div>
     )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,24 +1,24 @@
 // import 
 import style from './Navigation.module.css'
-
+import { NavLink } from 'react-router-dom'
 
 const Navigation = () => {
     return (
         <nav className={style.nav}>
             <div className={style.item}>
-                <a href="/profile">Profile</a>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/profile">Profile</NavLink>
             </div>
             <div className={style.item}>
-                <a href="/dialogs">Messages</a>
-            </div>
-            <div className={`${style.item} ${style.active}`}>
-                <a href="/news">News</a>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/dialogs">Messages</NavLink>
             </div>
             <div className={style.item}>
-                <a href="/music">Music</a>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/news">News</NavLink>
             </div>
             <div className={style.item}>
-                <a href="/settings">Settings</a>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/music">Music</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/settings">Settings</NavLink>
             </div>
         </nav>
     )   
